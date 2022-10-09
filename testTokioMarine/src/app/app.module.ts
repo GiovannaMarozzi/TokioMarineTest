2845e0fea1a96777a016a0536df35f9986a564da
Giovanna Marozzi <giovanna.machado99@hotmail.com>
2022-10-09 20:11:33 -0300

    import class AccountsService

--- testTokioMarine/src/app/app.module.ts
+++ testTokioMarine/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import {AccountsService} from "./accounts.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClientModule, AccountsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
