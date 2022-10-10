071604e9d155b450a3b393e796d50cc74d993d3a
Giovanna Marozzi <giovanna.machado99@hotmail.com>
2022-10-10 17:44:43 -0300

    Create interface

---
+++ TokioMarineTest/testTokioMarine/src/app/transfers/account.model.ts
@@ -0,0 +1,8 @@
+export class AccountModel{
+  id?: number;
+  name: string | undefined;
+  ctOrigin: number | undefined;
+  ctDest: number | undefined;
+  dtTransfer: string | undefined;
+  dtAgendamento: string | undefined;
+}
