01c1c400431a75746c38e5ba6a7731cd9dd8b7c3
Giovanna Marozzi <giovanna.machado99@hotmail.com>
2022-10-10 16:03:31 -0300

    Add new imports

--- TokioMarineTest/testTokioMarine/src/app/app.module.ts
+++ TokioMarineTest/testTokioMarine/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule} from "@angular/common/http";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransfersComponent } from './transfers/transfers.component';
+
+import {TransfersService} from "./transfers.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransfersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [HttpClientModule, TransfersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
