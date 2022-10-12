53717f9f9344fec281b5dd29aee619b43a61698e
Giovanna Marozzi <giovanna.machado99@hotmail.com>
2022-10-11 23:15:10 -0300

    testes

--- testTokioMarine/src/app/transfers/transfers.component.ts
+++ testTokioMarine/src/app/transfers/transfers.component.ts
@@ -12,16 +12,48 @@ export class TransfersComponent implements OnInit {
   account: AccountModel = new AccountModel();
   transfers: Array<any> = new Array<any>();
   public data: Date = new Date();
-  public valueFinal: any;
-
+  valueFinal: any;
 
   constructor(private transfersService: TransfersService) { }
 
+  date1 = new Date();
+
+  // currentYear =this.data.getFullYear()
+  // currentMonth = this.data.getMonth() +1;
+  // currentDay = this.data.getDate()
+  // //YYYY-MM-DD
+  // TodayDate = "2022-11-20";
+  //
+  // finalMonth: any;
+  // finalDay: any;
+
+
   ngOnInit(): void {
     this.listAccounts();
+
+    // if(this.currentMonth <10){
+    //   this.finalMonth = "0" + this.currentDay;
+    // }else{
+    //   this.finalMonth = this.currentDay;
+    // }
+    //
+    // if(this.currentDay <10){
+    //   this.finalDay = "0" + this.currentDay;
+    // }else{
+    //   this.finalDay = this.currentDay;
+    // }
+    //
+    // this.TodayDate = this.currentYear+"-"+this.finalMonth+"-"+this.finalDay;
+    // console.log(this.currentYear)
+    // console.log(this.finalMonth)
+    // console.log(this.finalDay)
+
+    // console.log(this.data.getDate())
+    // console.log(this.data.getMonth() +1)
+    // console.log(this.data.getFullYear())
   }
 
-  send(){
+  calculator(){
     const d1 = this.account.dtTransfer;
     const d2 = this.account.dtAgendamento;
     // @ts-ignore
@@ -57,10 +89,17 @@ export class TransfersComponent implements OnInit {
       this.valueFinal = value - (1.7/100);
     }else{
       alert("Erro ao realizar a transferência")
+      this.valueFinal = "Error"
+      this.account.valueFinal = this.valueFinal;
     }
+    console.log(this.data)
 
+
+  }
+
+  send(){
     this.transfersService.sendTransfer(this.account).subscribe(accounts =>{
-      // this.account = new AccountModel();
+      this.account = new AccountModel();
     }, error => {console.log(error)
     })
   }
